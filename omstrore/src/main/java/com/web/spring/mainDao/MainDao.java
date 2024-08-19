b3cf6346c19738968677273a46d2696af77c5fc4
johongsoo <ghdtn134@gmail.com>
2024-08-19 13:59:20 +0900

    Update MainDao.java

--- omstrore/src/main/java/com/web/spring/mainDao/MainDao.java
+++ omstrore/src/main/java/com/web/spring/mainDao/MainDao.java
@@ -2,6 +2,7 @@ package com.web.spring.mainDao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -24,4 +25,11 @@ public interface MainDao {
     
     @Select("SELECT CASE WHEN COUNT(*) > 0 THEN 1 ELSE 0 END FROM USERS WHERE userid = #{userid}")
     int selectId(String userid);
+    
+    @Insert("INSERT INTO Users (" +
+            "userid, name, pwd, email, birthdate, address, address1, address2, phone, rating, point" +
+            ") VALUES (" +
+            "#{userid}, #{name}, #{pwd}, #{email}, #{birthdate}, #{address}, #{address1}, #{address2}, #{phone}, #{rating}, #{point}" +
+            ")")
+    void registerUser(Users user);
 }
